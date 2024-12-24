103fb59649f46f39f59ef75fe77fc913dea5a499
budi-irawan <budi.irawan171717@gmail.com>
2024-12-24 15:32:55 +0700

    security - fitur lanjutar form - wizard

--- academic/__manifest__.py
+++ academic/__manifest__.py
@@ -7,12 +7,14 @@
     "website":"https://bootcamp.vitraining.com",
     "description": """Aplikasi Sistem Informasi Akademik""",
     "data":[
+        "security/groups.xml",
         "security/ir.model.access.csv",
         "menu.xml",
         "course.xml",
         "session.xml",
         "attendee.xml",
         "partner.xml",
+        "wizard/create_attendee.xml"
     ],
     "installable":True,
     "auto_install":False,
